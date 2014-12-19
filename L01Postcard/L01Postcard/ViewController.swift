46e2cf6057558a237e7920c45ded940a386ca496
Vladimir Roudakov <v_rouakov@yahoo.com>
2014-12-19 17:25:38 +1000

    First commit

---
+++ L01Postcard/L01Postcard/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  L01Postcard
+//
+//  Created by Vladimir Roudakov on 19/12/2014.
+//  Copyright (c) 2014 Vladimir Roudakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailBtnPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = messageTextField.text
+    }
+
+}
+
