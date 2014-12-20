eec14a1912cc34021a3738461c76c0f200c0289c
VladimirAus <v_roudakov@yahoo.com>
2014-12-21 01:24:04 +1000

    P02: operations & types

---
+++ P02Operations.playground/section-1.swift
@@ -0,0 +1,21 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+typealias WholeNumber = Int // Type aliases
+typealias NumberWithDecimal = Double // Type aliases
+
+var x:WholeNumber = 49, y = 10
+
+var plus = x + y
+var mult = x * y
+var sub = x - y
+var div = x / y
+
+var showsDiscount:NumberWithDecimal = 0.3
+var shoePrice:Double = 33
+var priceWithDisc = shoePrice * (1 - showsDiscount)
+
+var z = 33
+z = 44
+
+var largeNumber = 1_000_000_000_000
