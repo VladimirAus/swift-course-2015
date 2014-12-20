424707bdfe630fcd496cf1ca6cb3a69242b5e9f9
VladimirAus <v_roudakov@yahoo.com>
2014-12-21 01:45:20 +1000

    Adding second project: currecncy converter

---
+++ L02CurrencyConcerter/L02CurrencyConcerter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L02CurrencyConcerter
+//
+//  Created by Vladimir Roudakov on 21/12/2014.
+//  Copyright (c) 2014 Vladimir Roudakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
