ab1eae8e94cebf8c74864cc291adb6a9d6690c78
VladimirAus <v_roudakov@yahoo.com>
2014-12-21 01:58:08 +1000

    L02: conversion implemented

--- L02CurrencyConcerter/L02CurrencyConcerter/ViewController.swift
+++ L02CurrencyConcerter/L02CurrencyConcerter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var audNumTextField: UITextField!
+    @IBOutlet weak var usdNumLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertAudToUsd(sender: AnyObject) {
+        let audToUsd = 0.81
+        
+//        let audSum = audNumTextField.text
+//        let audSumCon = audSum.toInt()
+//        var audSumInt = audSumCon!
+        
+        let audSumDbl = Double((audNumTextField.text as NSString).doubleValue)
+        var usdSum = audToUsd * audSumDbl
+        
+        usdNumLbl.hidden = false
+        usdNumLbl.text = "$\(usdSum) USD"
+    }
 
 }
 
