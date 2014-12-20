6d99259515cdc33aa78e5e4f22873169c18ed442
VladimirAus <v_roudakov@yahoo.com>
2014-12-21 01:06:57 +1000

    P01: variables and comments

---
+++ Variables.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Single line comment
+var firstInt = 0, secondInt:Int = 29
+var thirdInt:Float = -1345890
+
+/*
+    Comments
+    multiline
+*/
+var discountPerc:Double = 0.3
+
