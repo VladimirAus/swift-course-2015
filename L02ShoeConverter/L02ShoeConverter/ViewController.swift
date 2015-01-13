19671fc06c47c22297891a3e9623fee641292064
VladimirAus <v_roudakov@yahoo.com>
2015-01-13 23:55:47 +1000

    App2: Shoe converter

---
+++ L02ShoeConverter/L02ShoeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L02ShoeConverter
+//
+//  Created by Vladimir Roudakov on 13/01/2015.
+//  Copyright (c) 2015 Vladimir Roudakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
