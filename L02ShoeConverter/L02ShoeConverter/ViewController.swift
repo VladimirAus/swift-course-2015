041877986f50294f61d9ef267ad0e865e4b13567
VladimirAus <v_roudakov@yahoo.com>
2015-01-14 00:00:34 +1000

    App2: interface setup

--- L02ShoeConverter/L02ShoeConverter/ViewController.swift
+++ L02ShoeConverter/L02ShoeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTxtfield: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertBtnPressed(sender: AnyObject) {
+    }
 
 }
 
