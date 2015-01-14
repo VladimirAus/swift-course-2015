291e0dae63ab7f7273916184779f781bcdc95e95
VladimirAus <v_roudakov@yahoo.com>
2015-01-14 23:54:33 +1000

    Playground for control flow with bools

--- P04ControlFlow.playground/section-1.swift
+++ P04ControlFlow.playground/section-1.swift
@@ -7,9 +7,20 @@ let lamboSpeed = 120
 var mySpeed:Int
 mySpeed = 80
 
-if mySpeed < 70 {
+if mySpeed < 70 || mySpeed >= 100 {
     println("Keep cruising")
+}
+else if mySpeed < 90 {
+    println("Getting there !")
 }
 else {
     println("Slow down!")
+}
+
+let isTvOn = true
+if (isTvOn == true) {
+    println("Switch off!")
+}
+else {
+    println("Switch on!")
 }
