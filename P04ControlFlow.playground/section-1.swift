8868db686f2f67d55fda6f935a0888189cf10295
VladimirAus <v_roudakov@yahoo.com>
2015-01-14 23:51:45 +1000

    Playground for control flow with if

---
+++ P04ControlFlow.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let truckSpeed = 45
+let lamboSpeed = 120
+var mySpeed:Int
+mySpeed = 80
+
+if mySpeed < 70 {
+    println("Keep cruising")
+}
+else {
+    println("Slow down!")
+}
