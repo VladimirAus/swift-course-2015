52ac04a51f081c8bdcd353d1327bc9eef842fb26
VladimirAus <v_roudakov@yahoo.com>
2015-01-15 00:40:21 +1000

    L04: adding tiger class

---
+++ L04LionsAndTigers/L04LionsAndTigers/Tiger.swift
@@ -0,0 +1,13 @@
+//
+//  Tiger.swift
+//  L04LionsAndTigers
+//
+//  Created by Vladimir Roudakov on 15/01/2015.
+//  Copyright (c) 2015 Vladimir Roudakov. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+
+}
