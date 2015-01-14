f0141939dffc5ee284f26c6e0740a3d74881234a
VladimirAus <v_roudakov@yahoo.com>
2015-01-15 00:16:26 +1000

    P05: Foor loops

---
+++ P05ForLoop.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var meditationHours = 1
+
+for meditationHours; meditationHours < 100; meditationHours = meditationHours + 1 {
+    println("Hours (\meditationHours)")
+}
+
+for var newMeditationHours = 100; newMeditationHours < 1; meditationHours-- {
+    println("More Hours (\newMeditationHours)")
+}
